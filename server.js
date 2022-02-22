da97a65b2b61a47cc1c6395e544bc0d8de31644e
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-02-23 00:11:13 +0100

    add mongodb

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
+const mongoose = require("mongoose");
 const app = require("./app");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3001;
+const DB_HOST = process.env.DB_HOST;
 
-app.listen(PORT, (error) => {
-  error && console.error("server error", error);
-  console.log(`Server running. Use our API on port: ${PORT}`);
-});
+(async () => {
+  try {
+    const response = await mongoose.connect(DB_HOST);
+    if (response) {
+      app.listen(PORT, (error) => {
+        console.log(`Server running. Use our API on port: ${PORT}`);
+        error && console.error(error.message);
+      });
+
+      console.log("Database connection successful");
+    }
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+})();
