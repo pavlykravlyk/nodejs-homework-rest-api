173ab452e022b69e21ff021c161eb6a11fd00e43
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-04-12 00:03:42 +0200

    Create register.js

---
+++ controllers/auth/register.js
@@ -0,0 +1,8 @@
+const register = (req, res) => {
+  //   const { email, password } = req.body;
+  //   const user = { email, password };
+  //   const token = jwt.sign(user, process.env.JWT_SECRET);
+  //   res.json({ token });
+};
+
+module.exports = register;
