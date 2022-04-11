cff31c15550f336e7f02596cdfe723e6e0bc480e
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-04-12 00:03:36 +0200

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: String,
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+    toObject: { virtuals: true },
+    toJSON: {
+      virtuals: true,
+      transform: (doc, ret) => {
+        delete ret._id;
+        return ret;
+      },
+    },
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
