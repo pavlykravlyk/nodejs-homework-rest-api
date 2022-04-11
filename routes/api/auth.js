ad4f68dc796e8bf277923b4eb1e2d8b80880dcdc
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-04-12 00:03:33 +0200

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,11 @@
 const express = require("express");
-
-const { contactSchema, favoriteSchema } = require("../../schemas");
-
 const router = express.Router();
+const { register } = require("../../controllers/auth");
+
+const validation = require("../../middelwares/validation");
+// const { contactSchema, favoriteSchema } = require("../../schemas");
+const { userSchema } = require("../../schemas");
+
+router.post("/register", validation(userSchema), register);
 
 module.exports = router;
