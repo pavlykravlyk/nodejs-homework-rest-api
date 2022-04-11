54d100b0ab664aa13132c8114e3603ae7064df58
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-04-11 21:23:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const logger = require("morgan");
 const cors = require("cors");
 
 const app = express();
+
+const authRouter = require("./routes/api/auth");
 const contactsRouter = require("./routes/api/contacts");
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 const HTTP_STATUS_CODES = require("./lib/constants");
@@ -10,7 +12,10 @@ const HTTP_STATUS_CODES = require("./lib/constants");
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
+
 app.use((req, res) => {
   res.status(HTTP_STATUS_CODES.NOT_FOUND).json({ message: "Not found" });
 });
