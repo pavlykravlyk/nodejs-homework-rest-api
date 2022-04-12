76ba6040030269791a262bdc1711fb4c22ce09cd
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-04-12 23:46:28 +0200

    Update user.js

--- schemas/user.js
+++ schemas/user.js
@@ -1,6 +1,6 @@
 const Joi = require("joi");
 
-const userSchema = Joi.object({
+const registerSchema = Joi.object({
   password: Joi.string()
     .required()
     .messages({ "string.empty": "Set password for user" }),
@@ -18,4 +18,18 @@ const userSchema = Joi.object({
     .default("starter"),
 });
 
-module.exports = userSchema;
+const loginSchema = Joi.object({
+  password: Joi.string()
+    .required()
+    .messages({ "string.empty": "Set password for user" }),
+
+  email: Joi.string()
+    .email({ minDomainSegments: 2 })
+    .pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$/)
+    .required()
+    .messages({
+      "string.email": "Invalid email address",
+    }),
+});
+
+module.exports = { registerSchema, loginSchema };
