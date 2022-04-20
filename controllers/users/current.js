0266684098b27603414a58153e84c9ab19bc2298
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-04-21 00:32:32 +0200

    Update current.js

--- controllers/users/current.js
+++ controllers/users/current.js
@@ -4,7 +4,8 @@ const { User } = require("../../models");
 const current = async (req, res) => {
   const { id } = req.user;
   const { email, subscription } = await User.findById(id);
-  res.json({
+
+  return res.json({
     status: "success",
     code: HTTP_STATUS_CODES.OK,
     data: { user: { email, subscription } },
