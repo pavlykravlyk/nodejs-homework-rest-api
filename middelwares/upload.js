c087d54a9d70a4e9f6144edc5e0288a3d7784907
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-04-28 16:00:39 +0200

    Update upload.js

--- middelwares/upload.js
+++ middelwares/upload.js
@@ -1,20 +1,20 @@
 const multer = require("multer");
 const path = require("path");
 
-const tempDir = path.join(__dirname, "../temp");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.join(__dirname, "../temp"));
+    },
 
-const multerConfig = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, tempDir);
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-  limits: {
-    fileSize: 2048,
-  },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+
+    limits: {
+      fileSize: 2048,
+    },
+  }),
 });
-
-const upload = multer({ storage: multerConfig });
 
 module.exports = upload;
