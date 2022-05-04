eb185c56aad344eefecced72cf821e59a5782984
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-05-04 21:41:26 +0200

    Update index.js

--- controllers/users/index.js
+++ controllers/users/index.js
@@ -2,5 +2,12 @@ const current = require("./current");
 const subscription = require("./subscription");
 const updateAvatar = require("./updateAvatar");
 const verifyEmail = require("./verifyEmail");
+const reVerifyEmail = require("./reVerifyEmail");
 
-module.exports = { current, subscription, updateAvatar, verifyEmail };
+module.exports = {
+  current,
+  subscription,
+  updateAvatar,
+  verifyEmail,
+  reVerifyEmail,
+};
