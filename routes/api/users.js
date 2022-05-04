e76e9d317b8a813ab340b00b65c6d8840bc122b8
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-05-04 21:41:33 +0200

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const { auth, validation, upload } = require("../../middelwares");
+const { subsSchema, verifySchema } = require("../../schemas");
 const {
   current,
   subscription,
   updateAvatar,
   verifyEmail,
+  reVerifyEmail,
 } = require("../../controllers/users");
-const { auth, validation, upload } = require("../../middelwares");
-const { subsSchema } = require("../../schemas");
 
 router.get("/current", auth, current);
+
 router.patch("/subscription", validation(subsSchema), auth, subscription);
 router.patch("/avatars", auth, upload.single("avatar"), updateAvatar);
+
 router.get("/verify/:verificationToken", verifyEmail);
+router.post("/verify", validation(verifySchema), reVerifyEmail);
 
 module.exports = router;
