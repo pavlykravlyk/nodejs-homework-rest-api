90c1187e0877669fdc5716d7f7370d92096f6d4f
Pavlo Kravchuk <kravchuk.pavlo@ukr.net>
2022-05-04 18:45:51 +0200

    Update register.js

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -2,6 +2,8 @@ const { User } = require("../../models");
 const HTTP_STATUS_CODES = require("../../lib/constants");
 const bcrypt = require("bcryptjs");
 const gravatar = require("gravatar");
+const { randomUUID } = require("crypto");
+const sendEmail = require("../../helpers");
 
 const register = async (req, res) => {
   const { email, password, subscription } = req.body;
@@ -12,19 +14,31 @@ const register = async (req, res) => {
       .status(HTTP_STATUS_CODES.CONFLICT)
       .json({ message: `User ${email} already exists` });
   }
+
   const avatarURL = gravatar.url(email);
   const hashedPassword = bcrypt.hashSync(password, 10);
+  const verificationToken = randomUUID();
   await User.create({
     email,
     password: hashedPassword,
     subscription,
     avatarURL,
+    verificationToken,
   });
+
+  const msg = {
+    to: email,
+    subject: "Email verification",
+    text: "Verify your email",
+    html: `<a target="_blank" href="http://localhost:${process.env.PORT}/api/users/verify/${verificationToken}">Click to verify</a>`,
+  };
+
+  await sendEmail(msg);
 
   res.status(HTTP_STATUS_CODES.CREATED).json({
     status: "success",
     code: HTTP_STATUS_CODES.CREATED,
-    data: { user: { email, subscription, avatarURL } },
+    data: { user: { email, subscription, avatarURL, verificationToken } },
   });
 };
 
